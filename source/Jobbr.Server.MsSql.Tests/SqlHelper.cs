0c831a92ad4e212b0a7ba61eb36265b191122187
Oliver Zürcher <oliver.zuercher@gmail.com>
2017-03-28 15:53:59 +0200

    wip unit tests

---
+++ source/Jobbr.Server.MsSql.Tests/SqlHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Jobbr.Server.MsSql.Tests
+{
+    public class SqlHelper
+    {
+        public static IEnumerable<string> SplitSqlStatements(string sqlScript)
+        {
+            var statements = sqlScript.Split(new[] { "GO" }, StringSplitOptions.RemoveEmptyEntries);
+
+            return statements
+                .Where(p => !string.IsNullOrWhiteSpace(p))
+                .Select(s => s.Trim(' ', '\r', '\n'));
+        }
+    }
+}

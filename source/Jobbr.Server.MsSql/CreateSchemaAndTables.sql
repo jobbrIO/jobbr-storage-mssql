40b19544de1460c3ec87f64118f7ec96ec121786
Oliver Zürcher <oliver.zuercher@gmail.com>
2017-03-29 11:43:40 +0200

    varchar fields changed to nvarchar

--- source/Jobbr.Server.MsSql/CreateSchemaAndTables.sql
+++ source/Jobbr.Server.MsSql/CreateSchemaAndTables.sql
@@ -10,18 +10,19 @@ CREATE TABLE [Jobbr].[JobRuns](
 	[Id] [bigint] IDENTITY(1,1) NOT NULL,
 	[JobId] [bigint] NOT NULL,
 	[TriggerId] [bigint] NOT NULL,
-	[JobParameters] [varchar](max) NULL,
-	[InstanceParameters] [varchar](max) NULL,
-	[Name] [varchar](50) NULL,
+	[JobParameters] [nvarchar](max) NULL,
+	[InstanceParameters] [nvarchar](max) NULL,
+	[Name] [nvarchar](50) NULL,
 	[PlannedStartDateTimeUtc] [datetime2](7) NOT NULL,
 	[ActualStartDateTimeUtc] [datetime2](7) NULL,
 	[EstimatedEndDateTimeUtc] [datetime2](7) NULL,
 	[ActualEndDateTimeUtc] [datetime2](7) NULL,
 	[Progress] [float] NULL,
-	[State] [varchar](15) NOT NULL,
+	[State] [nvarchar](15) NOT NULL,
 	[Pid] [int] NULL,
-	[WorkingDir] [varchar](300) NULL,
-	[TempDir] [varchar](300) NULL,
+  [Host] [nvarchar](100) NULL,
+	[WorkingDir] [nvarchar](300) NULL,
+	[TempDir] [nvarchar](300) NULL,
  CONSTRAINT [PK_JobRuns] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
@@ -39,10 +40,10 @@ SET ANSI_PADDING ON
 GO
 CREATE TABLE [Jobbr].[Jobs](
 	[Id] [bigint] IDENTITY(1,1) NOT NULL,
-	[UniqueName] [varchar](80) NOT NULL,
-	[Title] [varchar](150) NULL,
-	[Type] [varchar](200) NOT NULL,
-	[Parameters] [varchar](max) NULL,
+	[UniqueName] [nvarchar](80) NOT NULL,
+	[Title] [nvarchar](150) NULL,
+	[Type] [nvarchar](200) NOT NULL,
+	[Parameters] [nvarchar](max) NULL,
 	[CreatedDateTimeUtc] [datetime2](7) NOT NULL,
 	[UpdatedDateTimeUtc] [datetime2](7) NULL,
  CONSTRAINT [PK_Jobs] PRIMARY KEY CLUSTERED 
@@ -63,16 +64,16 @@ GO
 CREATE TABLE [Jobbr].[Triggers](
 	[Id] [bigint] IDENTITY(1,1) NOT NULL,
 	[JobId] [bigint] NOT NULL,
-	[TriggerType] [varchar](10) NOT NULL,
-	[Definition] [varchar](20) NULL,
+	[TriggerType] [nvarchar](10) NOT NULL,
+	[Definition] [nvarchar](20) NULL,
 	[StartDateTimeUtc] [datetime2](7) NULL,
 	[EndDateTimeUtc] [datetime2](7) NULL,
 	[DelayedMinutes] [int] NULL,
 	[IsActive] [bit] NOT NULL,
-	[UserId] [varchar](100) NULL,
-	[UserDisplayName] [varchar](100) NULL,
-	[Parameters] [varchar](max) NULL,
-	[Comment] [varchar](max) NULL,
+	[UserId] [nvarchar](100) NULL,
+	[UserDisplayName] [nvarchar](100) NULL,
+	[Parameters] [nvarchar](max) NULL,
+	[Comment] [nvarchar](max) NULL,
 	[CreatedDateTimeUtc] [datetime2](7) NOT NULL,
 	[NoParallelExecution] [bit] DEFAULT 0 NOT NULL
  CONSTRAINT [PK_Triggers] PRIMARY KEY CLUSTERED 

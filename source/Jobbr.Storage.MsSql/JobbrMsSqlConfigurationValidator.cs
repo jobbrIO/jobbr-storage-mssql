87eb9affb7a3a82a61fe94c0bba749713c689d36
Mara Huminiuc <mara.huminiuc@zuehlke.com>
2022-12-06 15:25:20 +0100

    Replace LibLog with Logging Abstractions

--- source/Jobbr.Storage.MsSql/JobbrMsSqlConfigurationValidator.cs
+++ source/Jobbr.Storage.MsSql/JobbrMsSqlConfigurationValidator.cs
@@ -1,12 +1,12 @@
-using System;
 using Jobbr.ComponentModel.Registration;
-using Jobbr.Server.MsSql.Logging;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace Jobbr.Storage.MsSql
 {
     internal class JobbrMsSqlConfigurationValidator : IConfigurationValidator
     {
-        private static readonly ILog Logger = LogProvider.For<MsSqlStorageProvider>();
+        private readonly ILogger<MsSqlStorageProvider> _logger;
 
         public Type ConfigurationType { get; set; } = typeof(JobbrMsSqlConfiguration);
 
@@ -19,14 +19,14 @@ namespace Jobbr.Storage.MsSql
 
             if (string.IsNullOrWhiteSpace(config.ConnectionString))
             {
-                Logger.Error("No connection string provided.");
+                _logger.LogError("No connection string provided.");
                 return false;
             }
 
             if (config.Retention.HasValue &&
                 config.Retention.Value < TimeSpan.FromDays(1))
             {
-                Logger.Error("Retention must be bigger than 1 day.");
+                _logger.LogError("Retention must be bigger than 1 day.");
                 return false;
             }
 
